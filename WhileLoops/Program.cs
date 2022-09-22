4c074cadf7ff44086dc67de3067a3faed691120f
Billy Dotson <billybad5@gmail.com>
2022-09-22 16:11:14 -0400

    Add project files.

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("While Loops!");
+
+// While Loop
+int index = 1;
+while (index <= 5)
+{
+    Console.WriteLine(index);
+    index++;
+}
+
+// Do While Loop
+int index1 = 6;
+do
+{
+    Console.WriteLine(index1);
+    index1++;
+} while (index1 <= 5);
